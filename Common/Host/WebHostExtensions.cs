8f05ddb44526e635135fecda5a7c70e281a955e6
Rob Pacheco <rob.pacheco@gmail.com>
2024-08-19 23:27:56 -0400

    Fixing merge

--- Common/Host/WebHostExtensions.cs
+++ Common/Host/WebHostExtensions.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Threading;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
-=======
->>>>>>> unit7/m2
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,31 +14,8 @@ namespace Skad.Common.Host
     {
         public static IHost MigrateDb(this IHost webHost)
         {
-<<<<<<< HEAD
             var migrations = webHost.Services.GetService<Migrations>();
             migrations.MigrateDb();
-=======
-            var config = webHost.Services.GetService<IConfiguration>();
-            var logger = webHost.Services.GetService<ILogger<IHost>>();
-
-            try
-            {
-                var cnx = new NpgsqlConnection(config.GetConnectionString("DefaultConnection"));
-                var evolve = new Evolve.Evolve(cnx, msg => logger.LogInformation(msg))
-                {
-                    Locations = new[] { "db/migrations" },
-                    IsEraseDisabled = true,
-                };
-
-                evolve.Migrate();
-                Thread.Sleep(120000);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError("Database migration failed.", ex);
-                throw;
-            }
->>>>>>> unit7/m2
             
             return webHost;
         }
